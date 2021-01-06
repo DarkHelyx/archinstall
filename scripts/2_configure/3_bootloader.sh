c26f4b2d309c8070fcd4f6fc136d81e3cf1c3da4
Haxalotl <haxxalotl@gmail.com>
2021-01-06 16:34:14 +1100

    Added bootloader entry file.

--- scripts/2_configure/3_bootloader.sh
+++ scripts/2_configure/3_bootloader.sh
@@ -8,9 +8,10 @@ if [ "${USER_CONSENT,,}" = "y" ]; then
         echo "default arch" >> /boot/loader/loader.conf &&
         echo "timeout 3" >> /boot/loader/loader.conf &&
         echo "console-mode max" >> /boot/loader/loader.conf &&
-        echo "editor no" >> /boot/loader/loader.conf
+        echo "editor 0" >> /boot/loader/loader.conf &&
+        cp $PWD/arch.conf /boot/loader/entries/arch.conf
 
-    printf "/boot/loader/loader.conf updated. Please read https://gist.github.com/OdinsPlasmaRifle/e16700b83624ff44316f87d9cdbb5c94 and update /boot/loader/entries/arch.conf."
+    printf "/boot/loader/loader.conf updated. Please update /boot/loader/entries/arch.conf.with UUID"
     exit
 else
     echo "Installation of bootloader aborted."
